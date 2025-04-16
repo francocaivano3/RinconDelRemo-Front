8c7132f0a42302af6ae87758d1716b9de46fc23e
AgusRey04 <reymundez37@gmail.com>
2025-04-16 18:29:57 -0300

    Creo Componente Perfil

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import LandingPage from './components/pages/landingPage/landingPage'
-
+import { useState } from "react";
+import LandingPage from "./components/pages/landingPage/landingPage";
+import Perfil from "./components/pages/perfil/Perfil";
 function App() {
-  
   return (
     <>
-      <LandingPage/>
+      <LandingPage />
+      <Perfil></Perfil>
     </>
-  )
+  );
 }
 
 export default App;
