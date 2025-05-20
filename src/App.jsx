914679b54311c31f8ae2e904b1e2d04a8bc5e8e2
mendieta967 <olorteguopedro@gmail.com>
2025-05-20 11:48:28 -0300

    10-administracion-de-usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
-
-import { useState } from 'react'
-import { ThemeProvider } from './components/context/themeContext/ThemeContext';
-import ThemeBtn from './components/ThemeBtn/ThemeBtn';
-import AlertProvider from './components/context/alertContext/AlertContext';
-import SimpleAlert from './components/alert/Alert';
-import LandingPage from './components/pages/landingPage/landingPage'; 
+import { ThemeProvider } from "./components/context/themeContext/ThemeContext";
+import ThemeBtn from "./components/ThemeBtn/ThemeBtn";
+import AlertProvider from "./components/context/alertContext/AlertContext";
+import SimpleAlert from "./components/alert/Alert";
+import LandingPage from "./components/pages/landingPage/landingPage";
 import Register from "../src/components/pages/register/Register";
 import Perfil from "./components/pages/perfil/Perfil";
+import SuperAdmin from "./components/pages/superAdmin/SuperAdmin";
 
 function App() {
-  return (
-    <ThemeProvider>
-      <AlertProvider>
-        <SimpleAlert/>
-        <Register/>
-        <ThemeBtn/>  
-      </AlertProvider>
-   </ThemeProvider>
-  );
+  return <SuperAdmin />;
 }
 
 export default App;
--- src/components/alert/Alert.jsx
+++ src/components/alert/Alert.jsx
@@ -1,23 +1,24 @@
-import Alert from '@mui/material/Alert';
-import { useAlert } from '../context/alertContext/AlertContext';
+//import Alert from "@mui/material/Alert";
+import { useAlert } from "../context/alertContext/AlertContext";
 
 export default function SimpleAlert() {
+  const { alert } = useAlert();
 
-    const {alert} = useAlert();
-    
-    if(!alert.message) return null;
+  if (!alert.message) return null;
 
   return (
-    <div style={{
+    <div
+      style={{
         position: "fixed",
         top: 20,
         right: 20,
         zIndex: 9999,
         minWidth: "250px",
-      }}>
-        <Alert severity={alert.type} variant='filled'>
-            {alert.message}
-        </Alert>
+      }}
+    >
+      <Alert severity={alert.type} variant="filled">
+        {alert.message}
+      </Alert>
     </div>
   );
 }
