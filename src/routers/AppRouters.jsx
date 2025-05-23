68c23db95c10f03ef98bcefc07061de2146bb7fb
marcostamburi <marcose027@hotmail.com>
2025-05-22 21:28:27 -0300

    Cambio modelo NavBar, Adapto nuevas rutas y el boton del tema

--- src/routers/AppRouters.jsx
+++ src/routers/AppRouters.jsx
@@ -1,43 +1,34 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import LandingPage from "../components/pages/landingPage/landingPage";
-import Register from "../components/pages/register/Register";
-import Perfil from "../components/pages/perfil/Perfil";
-import Dashboard from "../components/dashboard/Dashboard";
-import MyKayaks from "../components/pages/myKayaks/myKayaks";
-import SuperAdmin from "../components/pages/superAdmin/SuperAdmin"
-import NotFound from "../components/pages/notFound/NotFound";
-import Login from "../components/pages/login/Login"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from '../App';
 
-const AppRouter = () => {
-  const router = createBrowserRouter([
-    {
-      path: "/", 
-      element: <LandingPage />
-    },
-    {
-      path: "/login", element: <Login/>
-    },
-    {
-      path: "/register",
-      element: <Register/>
-    },
-    {
-      path: "/perfil", element: <Perfil/>
-    },
-    {
-      path: "/Dashboard", element: <Dashboard/>
-    },
-    {
-      path: "/my-kayaks", element: <MyKayaks/>
-    },
-    {
-      path: "/sysadmin", element: <SuperAdmin/>
-    },
-    {
-      path: "*", element: <NotFound/>
-    }
+// Páginas
+import LandingPage from '../components/pages/landingPage/landingPage';
+import Perfil from '../components/pages/perfil/Perfil';
+import MyKayaks from '../components/pages/myKayaks/myKayaks';
+import Register from '../components/pages/register/Register';
+import Login from '../components/pages/login/Login';
+import Dashboard from '../components/dashboard/Dashboard';
+import SuperAdmin from '../components/pages/superAdmin/SuperAdmin';
+import NotFound from '../components/pages/notFound/NotFound';
 
-  ]);
-  return <RouterProvider router={router} />;
-};
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />, // layout con navbar
+    children: [
+      { path: '/', element: <LandingPage /> },
+      { path: '/perfil', element: <Perfil /> },
+      { path: '/mis-kayaks', element: <MyKayaks /> },
+      { path: '/dashboard', element: <Dashboard /> },
+      { path: '/sysadmin', element: <SuperAdmin /> },
+      { path: '/register', element: <Register /> },
+      { path: '/historial', element: <NotFound /> },
+      { path: '*', element: <NotFound /> },
+      { path: '/login', element: <Login /> },
+    ],
+  },
+]);
+
+const AppRouter = () => <RouterProvider router={router} />;
+
 export default AppRouter;
